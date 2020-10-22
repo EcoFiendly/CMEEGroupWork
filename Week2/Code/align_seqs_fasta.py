11533a74f69498b92f990821e988b57d71fa57ed
bennouhan <bjn20@ic.ac.uk>
2020-10-22 14:16:03 +0100

    Updated alignseqsfasta

--- Week2/Code/align_seqs_fasta.py
+++ Week2/Code/align_seqs_fasta.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-"""Programme that takes the DNA sequences as an input from a single external file and saves
-the best alignment along with its corresponding score in a single text file
-!!!!!!But this includes partial overlap of strands at bth ends, not just latter!!!!!!"""
+"""Programme that takes the DNA sequences as an input from a single external
+file and saves the best alignment(s) along with its corresponding score in a
+single text file - but this includes partial overlap of strands at both ends,
+not just latter"""
 
 __author__ = 'Ben Nouhan (b.nouhan.20@imperial.ac.uk)'
 __version__ = '0.0.1'
@@ -12,7 +13,19 @@ import sys
 
 
 def get_seq(in_fpath):
-    """Reads the file entered as argument, adds each row as a string to a list called seqs"""
+    """Reads the file entered as argument, adds each row as a string to a list
+    called seqs
+    
+    Parameters:
+    
+    in_fpath - file path to fasta file used for data extraction
+    
+    
+    Returns:
+    
+    seq - fasta data extracted from input fasta file
+    
+    """
     seq = ""
     with open(in_fpath, "r") as f:
         seq_lines = f.read().splitlines(True)
@@ -21,8 +34,27 @@ def get_seq(in_fpath):
     return seq
 
 
-def calculate_score(s1, s2, l1, l2, startpoint): #need to make implicit loop to try each startpoint, choose highest score, give corresponding seqeunce side by side
-    """Computes score of the alignment given as parameters, 1 point per matching base pair"""
+def calculate_score(s1, s2, l1, l2, startpoint): 
+    """Computes score of the alignment given as parameters, 1 point per matching
+     base pair
+     
+    Parameters:
+    
+    s1 - longer input sequence, with (l2-1)*"."s appended to either end 
+    s2 - shorter input sequence
+    l1 - length of s1 prior to "."s appendage to either end
+    l2 - length of s2
+    startpoint - point along s1 at which alignment calc_score with s2 is occurrs
+    
+    
+    Returns:
+    
+    score - number of matched bases between s1 and s2
+    matched - sequence of "."s, "-"s and "*"s to annotate alignment
+    shift - "."s before s2 to indicate there are no matches between sequences
+    end_shift - "."s after s2 to indicate there are no matches between sequences
+    
+    """
     matched = "" # to hold string displaying alignements
     score = 0
     for i in range(l2):  
@@ -34,26 +66,33 @@ def calculate_score(s1, s2, l1, l2, startpoint): #need to make implicit loop to 
                 score = score + 1 #adds one to score
             else:
                 matched = matched + "-" #not matched bases
-    shift, end_shift = startpoint * ".", (l2 + l1 - startpoint - 2) * "." #dots at end, but only up until end of dots tailing l1    #if startpoint is bigger than l1-2, end shift is less than l2 according to this formula. the below check stops it from getting less than l2. is that necessary??
+    shift, end_shift = startpoint * ".", (l2 + l1 - startpoint - 2) * "."
+    #dots at end, but only up until end of dots tailing l1
+    #if startpoint is bigger than l1-2, end shift is less than l2 according to
+    # this formula. the below check stops it from getting less than l2.
     return score, matched, shift, end_shift
 
 
 def main(argv):
-    """Gets input from file, assigns longer seq to s1 & v.v., calculates scores, and saves highest-scoring alignment(s) in new file"""
+    """Gets input from files, assigns longer seq to s1 & vv, calculates scores,
+    and saves highest-scoring alignment(s) in new file with explanation"""
     
-    ### gets seqs from the two argued .fasta files, or if not provided gets seqs from the default files in data/
-    if len(sys.argv) == 3 and sys.argv[1].endswith("fasta") == True and sys.argv[2].endswith("fasta") == True:
+    ### gets seqs from the two argued .fasta files,
+    # or if not provided gets seqs from the default files in data/
+    if len(sys.argv) == 3 and sys.argv[1].endswith("fasta") == \
+    True and sys.argv[2].endswith("fasta") == True:
         print("Aligning input sequences... please wait.")
         seq1, seq2 = get_seq(sys.argv[1]), get_seq(sys.argv[2]) 
     else: #if not, inform them and use the default fasta files
-        print("Two .fasta files not provided. Using default files from data/fasta/.")
+        print("Two .fasta files not provided. Using defaults from data/fasta/.")
         seq1, seq2 = get_seq("../data/fasta/407228326.fasta"), get_seq("../data/fasta/407228412.fasta")
     
     
     ### Assign the longer sequence to s1, and the shorter to s2
     l1, l2 = len(seq1), len(seq2)
     if l1 >= l2:
-        s1, s2 = ((l2 - 1) * "." + seq1 + (l2 - 1) * "."), seq2 #puts l2-1 "."s either side of l1
+        s1, s2 = ((l2 - 1) * "." + seq1 + (l2 - 1) * "."), seq2
+        #puts l2-1 "."s both sides of l1, allows alignment of all overlap combos
     else:
         s1, s2 = ((l1 - 1) * "." + seq2 + (l1 - 1) * "."), seq1
         l1, l2 = l2, l1 
@@ -63,8 +102,12 @@ def main(argv):
     for i in range(l1 + l2 -1):
         score, matched, shift, end_shift = calculate_score(s1, s2, l1, l2, i)
         #assigns returns from calc_score function to these variables
-        statement = "This alignment occurs when the smaller strand (" + str(l2) + "nt in length) attaches from base " + str(i - l2 + 2) + " of the larger strand, with the highest score of " + str(score) + "." + "\n\n"
-        best_comparison_highSP, best_comparison_lowSP, best_s2, best_s1 = (shift + matched + (l2 - 1) * "." + "\n"), (shift + matched + end_shift + "\n"), (shift + s2 + end_shift + "\n"), (s1 + "\n\n\n\n")
+        statement = "This alignment occurs when the smaller strand (" + str(l2) + "nt in length) attaches from base " \
+        + str(i - l2 + 2) + " of the larger strand, with the highest score of " + str(score) + ":" + "\n"
+        #statement explaining the alignment in detail
+        best_comparison_highSP, best_comparison_lowSP, best_s2, best_s1 = (shift + matched + (l2 - 1) * "." + "\n"), \
+        (shift + matched + end_shift + "\n"), (shift + s2 + end_shift + "\n"), (s1 + "\n\n\n\n")
+        #formats the matching, s1 and s2 lines to line-up neatly
         if i < l1 - 1:
             best_alignment = (str(statement) + str(best_comparison_lowSP) + str(best_s2) + str(best_s1))
         else:
@@ -79,10 +122,10 @@ def main(argv):
         elif score == my_best_score:
             f = open('../results/fasta_align.txt', 'a')
             f.write(best_alignment)
-            f.close()
-        print("Done!")
-                
-
+            f.close()     
+    print("Done!")
+        
+        
 if (__name__ == "__main__"):
     status = main(sys.argv)
     sys.exit(status)
