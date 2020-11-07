dfc40858816787ce393e302e78e88de21b9544c1
CongLiu37 <72556150+CongLiu37@users.noreply.github.com>
2020-11-07 23:38:34 +0800

    Add files via upload

---
+++ Week3/final_get_TreeHeight.py
@@ -0,0 +1,51 @@
+"""Language: Python3
+   Author: Group 4
+   Script: get_TreeHeight.py
+   Work Path: CMEECourseWork/Week3
+   Dependencies: sys, math
+   Input: Data/trees.csv
+   Function: Calculate heights of trees given distance of each tree 
+             from its base and angle to its top, using  the trigonometric formula 
+   Output: Results/trees_treeheights.csv
+   Usage: python get_TreeHeight.py [Input]
+   Date: Oct, 2020"""
+
+import sys
+import math
+
+inpu = sys.argv[1]
+file = open(inpu, "r")
+
+data = []
+for line in file:
+    line = line.strip().split(",")
+    data.append(line)
+file.close()
+
+def TreeHeight(distance, degree):
+    """Calculate heights of trees.
+       
+       Input:
+       distance: the distance from tree base
+       degree: angle to its top
+       
+       Output: Height of tree"""
+    pi = math.pi
+    deg = degree*pi/180
+    tanv = math.tan(deg)
+    height = distance * tanv
+    return height
+
+res = []
+data[0].append("Tree.Height.m")
+for i in range(1,len(data)):
+    height = TreeHeight(float(data[i][1]), float(data[i][2]))
+    data[i].append(height)
+
+result_name = "Results/" + inpu.split("/")[1].split(".")[0] + "_treeheights.csv"
+result = open(result_name, "w")
+for i in data:
+    result.write(i[0]+","+i[1]+","+i[2]+","+str(i[3]))
+    result.write("\n")
+
+result.close() 
---
+++ Week3/final_get_TreeHeight_final.R
@@ -0,0 +1,110 @@
+# Author: Group 4
+#
+# Script: get_TreeHeight.R
+#
+# Desc: Reads argued .csv file(s) for data frame, adds blank 4th column, uses
+#       columns 2 & 3 to calculate tree heights to populate 4th column, then
+#       writes new dataframe into a new .csv file
+#
+# Arguments:
+# .csv file(s) with second header "Distance.m" and third header "Angle.degrees", 
+# or none to use default.
+#
+# Output:
+# ../results/[argument basename]_treeheights.csv
+#
+# Date: 27 Oct 2020
+
+
+
+### This function calculates heights of trees given distance of each tree 
+# from its base and angle to its top, using  the trigonometric formula 
+#
+# height = distance * tan(radians)
+#
+#
+# ARGUMENTS
+#
+# degrees:   The angle of elevation of tree
+# distance:  The distance from base of tree (e.g., meters)
+#
+#
+# RETURN
+#
+# height:    The heights of the tree, same units as "distance"
+#
+TreeHeight <- function(degrees, distance){
+    radians <- degrees * pi / 180
+    height <- distance * tan(radians)
+    return (height)
+}
+
+
+### This function gets the datagrame from the argued .csv file, creates a 4th
+# column, uses TreeHeight() to populate this column with treeheights using
+# columns 2 & 3, then writes the expanded dataframe into a custom-named .csv
+# file in ../results/
+#
+#
+# ARGUMENTS
+#
+# filename.csv: .csv file(s) with second header "Distance.m" and third header
+#               "Angle.degrees"
+#
+#
+# RETURN
+#
+# NULL
+#
+getCSV_addTreeHeight <- function(filename.csv){
+    MyData <- read.csv(filename.csv, header = TRUE)
+    MyData["Tree.Height.m"] <- NA
+
+    MyData[,4] <- TreeHeight(MyData[,3], MyData[,2])
+    
+    basename <- tools::file_path_sans_ext(basename(filename.csv))
+    #tool removes extension, basename() removes filepath
+    new_fpath <- paste("../results/",basename,"_treeheights.csv", sep = "")
+    write.csv(MyData, new_fpath)
+    return (NULL)
+}
+
+
+
+term_args <- commandArgs(trailingOnly = TRUE); csv_count <- 0
+
+for (i in term_args){
+    if (endsWith(i, ".csv") & file.exists(i)){ # Checks i exists and is a .csv
+        # Checks the table in i is compatible with getCSV_addTreeHeight()
+        header_check <- read.csv(i, header = FALSE)
+        if (toString(header_check[1,2]) == "Distance.m" &
+            toString(header_check[1,3]) == "Angle.degrees"){
+                csv_count <- csv_count + 1
+                getCSV_addTreeHeight(i); cat("Finished converting", i, "\n")
+
+        } else {
+            (cat(paste(i, "is incompatible. The second header must be",
+            "'Distance.m' and the third header 'Angle.degrees'\n")))
+        }
+    } else {
+        (cat(i, "is not an existing .csv file; please enter a .csv file", "\n"))
+    }
+
+}
+
+if (length(term_args) == 0 || csv_count == 0) { # If no file converted or argued
+    cat("No appropriate .csv file entered, using default: ../data/trees.csv\n")
+    getCSV_addTreeHeight("../data/trees.csv")
+    cat("Finished converting ../data/trees.csv\n")
+}
+
+
+
+
+
+#     Write a Unix shell script called run_get_TreeHeight.sh that tests 
+#     get_TreeHeight.R. Include trees.csv as your example file. Note that source will
+#      not work in this case as it does not allow scripts with arguments to be run; 
+#      you will have to use Rscript instead.
+
+#NOTE FROM BEN    showcase the checks you've made in the shell script?
